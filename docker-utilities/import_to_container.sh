8179918918edf3a2a4f4b2c46c423b917788fc12
AlfredoKCL <alfredo.iacoangeli@gmail.com>
2017-05-16 00:23:01 +0100

    useful scripts for docker

---
+++ docker-utilities/import_to_container.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` <containerID> <path_to_folder_to_import> <path_to_folder_to_destination>"
+  exit 0
+fi
+
+
+
+CONTAINER=$1
+HOSTPATH=$2
+CONTPATH=$3
+
+REALPATH=$(readlink --canonicalize $HOSTPATH)
+FILESYS=$(df -P $REALPATH | tail -n 1 | awk '{print $6}')
+
+while read DEV MOUNT JUNK
+do [ $MOUNT = $FILESYS ] && break
+done </proc/mounts
+[ $MOUNT = $FILESYS ] # Sanity check!
+
+while read A B C SUBROOT MOUNT JUNK
+do [ $MOUNT = $FILESYS ] && break
+done < /proc/self/mountinfo 
+[ $MOUNT = $FILESYS ] # Moar sanity check!
+
+SUBPATH=$(echo $REALPATH | sed s,^$FILESYS,,)
+DEVDEC=$(printf "%d %d" $(stat --format "0x%t 0x%T" $DEV))
+
+docker-enter $CONTAINER  sh -c \
+	     "[ -b $DEV ] || mknod --mode 0600 $DEV b $DEVDEC"
+docker-enter $CONTAINER  mkdir /tmpmnt
+docker-enter $CONTAINER  mount $DEV /tmpmnt
+docker-enter $CONTAINER  mkdir -p $CONTPATH
+docker-enter $CONTAINER  mount -o bind /tmpmnt/$SUBROOT/$SUBPATH $CONTPATH
+docker-enter $CONTAINER  umount /tmpmnt
+docker-enter $CONTAINER  rmdir /tmpmnt
